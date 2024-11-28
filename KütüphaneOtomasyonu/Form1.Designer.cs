f2d9fbd62d35402cda07619b944b9197dbe5bce0
Sefacan Aksu <119726095+sefcanaksu@users.noreply.github.com>
2024-11-28 12:52:05 +0300

    Add files via upload

---
+++ KütüphaneOtomasyonu/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace KutuphaneOtomasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(66, 60);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Kitap Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(66, 115);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Kitap Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Kitap Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ KütüphaneOtomasyonu/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using KutuphaneOtomasyonu;
+
+namespace KutuphaneOtomasyonu
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection con;
+        private OleDbDataAdapter da;
+        private DataTable dt;
+
+        public Form1()
+        {
+            InitializeComponent();
+            VeritabaniBaglanti();
+            KitaplariListele();
+        }
+
+        private void VeritabaniBaglanti()
+        {
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Kutuphane.accdb";
+            con = new OleDbConnection(connectionString);
+        }
+
+        private void KitaplariListele()
+        {
+            try
+            {
+                con.Open();
+                string query = "SELECT * FROM Kitaplar";
+                da = new OleDbDataAdapter(query, con);
+                dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt; // dataGridView1: Formdaki bir DataGridView kontrolü
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void KitapEkle(string kitapAdı, string yazar, string yayıncı, string durum)
+        {
+            try
+            {
+                con.Open();
+                string query = "INSERT INTO Kitaplar (KitapAdı, Yazar, Yayıncı, Durum) VALUES (@KitapAdı, @Yazar, @Yayıncı, @Durum)";
+                OleDbCommand cmd = new OleDbCommand(query, con);
+                cmd.Parameters.AddWithValue("@KitapAdı", kitapAdı);
+                cmd.Parameters.AddWithValue("@Yazar", yazar);
+                cmd.Parameters.AddWithValue("@Yayıncı", yayıncı);
+                cmd.Parameters.AddWithValue("@Durum", durum);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Kitap başarıyla eklendi!");
+                KitaplariListele();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void KitapSil(int kitapID)
+        {
+            try
+            {
+                con.Open();
+                string query = "DELETE FROM Kitaplar WHERE KitapID = @KitapID";
+                OleDbCommand cmd = new OleDbCommand(query, con);
+                cmd.Parameters.AddWithValue("@KitapID", kitapID);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Kitap başarıyla silindi!");
+                KitaplariListele();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void KitapGuncelle(int kitapID, string kitapAdı, string yazar, string yayıncı, string durum)
+        {
+            try
+            {
+                con.Open();
+                string query = "UPDATE Kitaplar SET KitapAdı = @KitapAdı, Yazar = @Yazar, Yayıncı = @Yayıncı, Durum = @Durum WHERE KitapID = @KitapID";
+                OleDbCommand cmd = new OleDbCommand(query, con);
+                cmd.Parameters.AddWithValue("@KitapAdı", kitapAdı);
+                cmd.Parameters.AddWithValue("@Yazar", yazar);
+                cmd.Parameters.AddWithValue("@Yayıncı", yayıncı);
+                cmd.Parameters.AddWithValue("@Durum", durum);
+                cmd.Parameters.AddWithValue("@KitapID", kitapID);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Kitap başarıyla güncellendi!");
+                KitaplariListele();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        // Örnek buton click event
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            KitapEkle("Test Kitap", "Yazar Adı", "Yayınevi", "Mevcut");
+        }
+    }
+}
